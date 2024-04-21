88055c9389c24ef0133a796caa984d3047c75088
Nelson Feijó <114948173+nelsonfeijo@users.noreply.github.com>
2024-04-21 15:25:20 -0300

    header e home

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('#mobile_btn').on('click', function(){
-        $('mobile_menu').toggLeClass
+        $('#mobile_menu').toggLeClass('active');
+        $('#mobile_btn').toggLeClass('fa-x')
     })
 })
